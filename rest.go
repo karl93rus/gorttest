be5548c6ef081f47afa3ba1c157948df4cc607f2
Aleksandr Sazonov <aleksandr.sazonov@south.rt.ru>
2019-08-20 09:32:59 +0300

    logging

--- rest.go
+++ rest.go
@@ -6,16 +6,17 @@ import (
   "database/sql"
   "strings"
   "time"
+  "os"
 )
 
 func getCodeByName(w http.ResponseWriter, req *http.Request) {
   country := req.URL.Path[len("/code/"):]
   country = strings.ToLower(country)
+  fmt.Println(req.Method, time.Now(), req.RemoteAddr)
   if req.Method != "GET" {
     fmt.Fprintf(w, "Invalid request method. Terminating...")
     return
   }
-  fmt.Println(req.Method, time.Now())
 
   connStr := "user=karl dbname=karl host=localhost sslmode=disable"
   db, err := sql.Open("postgres", connStr)
@@ -50,4 +51,13 @@ func getCodeByName(w http.ResponseWriter, req *http.Request) {
 
   w.WriteHeader(200)
   fmt.Fprintf(w, "hello blya, %v, %v", r.c, r.n)
+
+  var logStr string
+  logStr = req.Method + " from " + req.RemoteAddr + " on " + string(req.URL.Path) + " at " + time.Now().Format("2006-01-02 15:04:05")
+  file, err := os.OpenFile("mainlog.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+  if err != nil {
+    fmt.Println("Error opening file...")
+  }
+  defer file.Close()
+  file.Write([]byte(logStr + "\n"))
 }
--- updateDb.go
+++ updateDb.go
@@ -4,6 +4,8 @@ import (
   "fmt"
   "net/http"
   _ "github.com/lib/pq"
+  "os"
+  "time"
 )
 
 func updateDb(w http.ResponseWriter, req *http.Request) {
@@ -16,10 +18,19 @@ func updateDb(w http.ResponseWriter, req *http.Request) {
   }
   connStr := "user=karl dbname=karl host=localhost sslmode=disable"
 
-  fmt.Fprintf(w, "Updating...........")
+  fmt.Fprintf(w, "Updating...........\n")
 
   getCountries(connStr)
   getCodes(connStr)
 
   fmt.Fprintf(w, "Updated")
+
+  var logStr string
+  logStr = "UPDATE DATABASE " + req.Method + " from " + req.RemoteAddr + " on " + string(req.URL.Path) + " at " + time.Now().Format("2006-01-02 15:04:05")
+  file, err := os.OpenFile("mainlog.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+  if err != nil {
+    fmt.Println("Error opening file...")
+  }
+  defer file.Close()
+  file.Write([]byte(logStr + "\n"))
 }
